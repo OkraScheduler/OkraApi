72a310755008bbeee8c082874d8ce7529fbcdcbf
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2017-05-30 22:17:03 -0300

    Adding Spring API

---
+++ src/main/java/okra/base/spring/AbstractOkraSpring.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017 Okra Scheduler
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+package okra.base.spring;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+import okra.base.AbstractOkra;
+import okra.base.model.OkraItem;
+
+@EqualsAndHashCode(callSuper = true)
+@Setter(AccessLevel.NONE)
+@Data
+public abstract class AbstractOkraSpring<T extends OkraItem> extends AbstractOkra<T> implements OkraSpring<T> {
+
+    private final String database;
+    private final String collection;
+
+    public AbstractOkraSpring(final String database, final String collection) {
+        this.collection = collection;
+        this.database = database;
+    }
+}
---
+++ src/main/java/okra/base/spring/OkraSpring.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2017 Okra Scheduler
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+package okra.base.spring;
+
+import okra.base.Okra;
+import okra.base.model.OkraItem;
+import okra.base.model.OkraStatus;
+import okra.exception.OkraItemNotFoundException;
+
+import java.util.Map;
+import java.util.Optional;
+
+public interface OkraSpring<T extends OkraItem> extends Okra<T> {
+
+    /**
+     * Retrieves a scheduled item from the backend, deleting it if successful.
+     * <p>
+     * Warning: The item processing is not guaranteed.
+     * If you want to delete the item after the processing is finished in order to
+     * do not delete any items before the processing is done, use method {@link OkraSpring#peek()}
+     *
+     * @return An optional containing the item if found, otherwise an empty optional
+     */
+    Optional<T> poll();
+
+    /**
+     * Retrieves a scheduled item from the backend.
+     * <p>
+     * This method doesn't deletes the item if any is found.
+     * Your should call {@link OkraSpring#delete(OkraItem)} after the processing is finished.
+     *
+     * @return An optional containing the item if found, otherwise an empty optional
+     */
+    Optional<T> peek();
+
+    /**
+     * Retrieves, but does not remove, the head of queue. This method
+     * differs from {@link OkraSpring#peek peek} only in that it throws an exception
+     * if there is no elements available.
+     *
+     * @return An optional containing the item if found, otherwise an empty optional
+     */
+    T retrieve() throws OkraItemNotFoundException;
+
+    /**
+     * Reschedule an item that was previously retrieved from the scheduled items pool
+     *
+     * @param item The item that will be rescheduled
+     * @return The rescheduled item if success, otherwise an empty optional
+     */
+    Optional<T> reschedule(T item);
+
+    /**
+     * Heartbeat an item to prevent that other scheduled item consumers acquire this same item
+     *
+     * @param item The item to heartbeat
+     * @return The updated item if success, otherwise an empty optional
+     */
+    Optional<T> heartbeat(T item);
+
+    /**
+     * Heartbeat an item to prevent that other scheduled item consumers acquire this same item
+     * This operation also updates custom attributes
+     *
+     * @param item The item to heartbeat
+     * @return The updated item if success, otherwise an empty optional
+     */
+    Optional<T> heartbeatAndUpdateCustomAttrs(T item, Map<String, Object> attrs);
+
+    /**
+     * Deletes a scheduled item
+     *
+     * @param item The item to be deleted
+     */
+    void delete(T item);
+
+    /**
+     * Schedules an item
+     *
+     * @param item The item to schedule
+     */
+    void schedule(T item);
+
+    /**
+     * Count items by status.
+     * <p>
+     * e.g. how many items are pending? Or How many items are currently processing?
+     *
+     * @param status
+     * @return The count
+     */
+    long countByStatus(OkraStatus status);
+
+    /**
+     * Counts the number of delayed items that are waiting to be processed.
+     *
+     * @return the count
+     */
+    long countDelayed();
+}
